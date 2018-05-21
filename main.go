478906896e931ec6bfbcf7f8a4b70f87371313bd
Michael Wuertinger <michael@wuertinger.de>
2018-05-21 12:34:04 +0200

    Refactoring and error handling
    - Refactored the overall program structure to make connection handling clearer
    - Cleaned up logging: Only show connect and close messages in normal operation
    - Improved error handling: Only display errors indicating abnormal program states

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 	"strconv"
+	"sync/atomic"
+	"syscall"
 	"time"
 )
 
@@ -22,6 +25,7 @@ func main() {
 		printUsageAndExit(fmt.Sprintf("%s is not an integer", os.Args[3]))
 	}
 
+	var shuttingDown uint32
 	shutdown := make(chan os.Signal)
 	signal.Notify(shutdown, os.Interrupt, os.Kill)
 
@@ -30,114 +34,131 @@ func main() {
 		log.Fatalf("listen: %v", err)
 	}
 
-	go acceptConnections(listener, shutdown, forward, throughput)
+	go server(listener, &shuttingDown, forward, throughput)
 
 	<-shutdown
+	atomic.StoreUint32(&shuttingDown, 1)
 	err = listener.Close()
 	if err != nil {
 		log.Printf("close: %v", err)
 	}
 }
 
-func acceptConnections(listener net.Listener, shutdown chan os.Signal, forward string, throughput int) {
+// server accepts new connections and forwards them accordingly to the forward address limiting the throughput (bytes
+// per second). The integer shuttingDown is used as a flag to indicate that the process is shutting down.
+func server(listener net.Listener, shuttingDown *uint32, forward string, throughput int) {
 	for {
-		conn, err := listener.Accept()
+		incomingConn, err := listener.Accept()
+		if atomic.LoadUint32(shuttingDown) != 0 { // if the process is shutting down we can ignore the error if any
+			return
+		}
 		if err != nil {
 			log.Printf("accept: %v", err)
-			shutdown <- nil
+			continue
+		}
+
+		// set the buffer size to the throughput (bytes/second) because it does not make sense to read more than
+		// one second worth of data ahead
+		bufSize := throughput
+
+		forwardConn, err := net.Dial("tcp", forward)
+		if err != nil {
+			log.Printf("unable to dial: %v", err)
+			if err := incomingConn.Close(); err != nil {
+				log.Printf("%v: unexpected error: %v", incomingConn.RemoteAddr(), err)
+			}
+			continue
+		}
+
+		connTcp := incomingConn.(*net.TCPConn)
+		forwardConnTcp := forwardConn.(*net.TCPConn)
+
+		setTcpConnBuffers(connTcp, bufSize)
+		setTcpConnBuffers(forwardConnTcp, bufSize)
+
+		log.Print(connTcp.RemoteAddr(), " open")
+
+		go slowCopy(forwardConnTcp, connTcp, throughput, bufSize)
+		go slowCopy(connTcp, forwardConnTcp, throughput, bufSize)
+	}
+}
+
+// setTcpConnBuffers adjusts the connection read and write buffer sizes to the specified value.
+func setTcpConnBuffers(conn *net.TCPConn, bufSize int) {
+	conn.SetReadBuffer(bufSize)
+	conn.SetWriteBuffer(bufSize)
+}
+
+// slowCopy works like io.Copy but limits the throughput to the specified value (in bytes per second) and reads no more
+// than bufSize at a time.
+func slowCopy(w *net.TCPConn, r *net.TCPConn, throughput, bufSize int) {
+	buf := make([]byte, bufSize, bufSize)
+	for {
+		start := time.Now()
+		size, err := r.Read(buf)
+		if err == io.EOF || isBrokenPipe(err) {
+			log.Printf("%v: closed", r.RemoteAddr())
+			w.CloseWrite()
+			return
+		}
+		if err != nil {
+			log.Printf("%v: unexpected error: %v", r.RemoteAddr(), err)
+			w.Close()
+			r.Close()
 			return
 		}
 
-		handleConnection(conn, forward, throughput)
+		_, err = w.Write(buf[0:size])
+		if err == io.EOF || isBrokenPipe(err) {
+			log.Printf("%v: closed", w.RemoteAddr())
+			r.CloseRead()
+			return
+		}
+		if err != nil {
+			log.Printf("%v: unexpected error: %v", w.RemoteAddr(), err)
+			w.Close()
+			r.Close()
+			return
+		}
+
+		delay(throughput, size, time.Since(start))
 	}
 }
 
-func handleConnection(conn net.Conn, forward string, throughput int) {
-	// set the buffer size to the throughput (bytes/second) because it does not make sense to read more than
-	// one second worth of data ahead
-	bufSize := throughput
-
-	if connTcp, ok := conn.(*net.TCPConn); ok {
-		connTcp.SetReadBuffer(bufSize)
-		connTcp.SetWriteBuffer(bufSize)
+// isBrokenPipe determines if err was caused by an EPIPE error.
+func isBrokenPipe(err error) bool {
+	opErr, ok := err.(*net.OpError)
+	if !ok {
+		return false
+	}
+	syscallErr, ok := opErr.Err.(*os.SyscallError)
+	if !ok {
+		return false
+	}
+	errno, ok := syscallErr.Err.(syscall.Errno)
+	if !ok {
+		return false
+	}
+	if errno == syscall.EPIPE {
+		return true
 	} else {
-		panic(fmt.Sprint("non-TCPConn connection: ", conn))
+		log.Printf("errno: 0x%x", errno)
+		return false
 	}
-
-	forwardConn, err := net.Dial("tcp", forward)
-	if err != nil {
-		log.Printf("dial: %v", err)
-		if err := conn.Close(); err != nil {
-			log.Printf("conn.close: %v", err)
-		}
-		return
-	}
-	if connTcp, ok := forwardConn.(*net.TCPConn); ok {
-		connTcp.SetReadBuffer(bufSize)
-		connTcp.SetWriteBuffer(bufSize)
-	} else {
-		panic(fmt.Sprint("non-TCPConn connection: ", forwardConn))
-	}
-
-	go func() {
-		buf := make([]byte, bufSize, bufSize)
-		for {
-			start := time.Now()
-			size, err := conn.Read(buf)
-			if err != nil {
-				log.Printf("conn.read: %v", err)
-				conn.Close()
-				forwardConn.Close()
-				return
-			}
-
-			_, err = forwardConn.Write(buf[0:size])
-			if err != nil {
-				log.Printf("forwardConn.write: %v", err)
-				conn.Close()
-				forwardConn.Close()
-				return
-			}
-
-			delay(throughput, size, time.Since(start))
-		}
-	}()
-
-	go func() {
-		buf := make([]byte, bufSize, bufSize)
-		for {
-			start := time.Now()
-			size, err := forwardConn.Read(buf)
-			if err != nil {
-				log.Printf("forwardConn.read: %v", err)
-				conn.Close()
-				forwardConn.Close()
-				return
-			}
-
-			_, err = conn.Write(buf[0:size])
-			if err != nil {
-				log.Printf("conn.write: %v", err)
-				conn.Close()
-				forwardConn.Close()
-				return
-			}
-
-			delay(throughput, size, time.Since(start))
-		}
-	}()
 }
 
-func delay(throughput, size int, actualDelay time.Duration) {
+// delay sleeps for the appropriate amount of time in order to simulate throughput. It requires the amount of
+// transmitted data and the time it took (transmissionDuration) in order to calculate the pause time.
+func delay(throughput, transmitted int, transmissionDuration time.Duration) {
 	// calculate the relative number of bytes in relation to the allowed throughput
-	share := float64(size) / float64(throughput)
+	share := float64(transmitted) / float64(throughput)
 
 	// calculate how long that should have taken
 	expectedDelay := time.Duration(share*1000.0) * time.Millisecond
 
 	// Sleep the remaining amount of time if necessary
-	if actualDelay < expectedDelay {
-		time.Sleep(expectedDelay - actualDelay)
+	if transmissionDuration < expectedDelay {
+		time.Sleep(expectedDelay - transmissionDuration)
 	}
 }
 
